e5f6647c0a840e899e97c49cc2e08c8520e2de75
Devin Moure <devin.moure@rlcarriers.com>
2025-01-07 09:48:05 -0500

    Fix call

--- server.js
+++ server.js
@@ -32,28 +32,31 @@ app.post('/webhook', (req, res) => {
 });
 
 app.get('/email', async (req, res) => {
-  const url = 'https://https://staging.gohighway.com/core/connect/external_api/v1/carriers/email_search_associated_carriers';
-  
-  const options = {
-    method: 'POST',
-    headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      Authorization: 'Bearer ' + process.env.HIGHWAYAPITOKEN
-    },
-    data: {
-      email: 'test@example.com'
-    }
-  };
 
-  try {
-    const response = await axios(url, options);
-    console.log(response.data);
-    res.json(response.data); // Send the response data back to the client
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  let data = qs.stringify({
+    'email': 'test@gmail.com' 
+  });
+  
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://highway.com/core/connect/external_api/v1/carriers/email_search_associated_carriers',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Authorization': 'Bearer' + process.env.HIGHWAYAPIKEY, 
+      'Cookie': '_highway_etl_session=MKCFiKKsnMoEkkOb6r7MxGkgCSZNrm0M6TVdGZqKBXl5s41dyYC4AJQLrsEvHFbJZwWdbGAYG6bgv2azZY%2FxNzkqDjy5UbWgNOeTNIfbPojOJbXB3E9snQHCwSnAnGJM65gIaWca9Ob%2B4T6zX4THFcS53siupeXpCHpkw%2FR3GVH5EM%2BSYAXnXaPwRDrAmKETisxHAiUFAhyAmTGOXnM4xg8kQtISpZaTix7vf2xn0Gw%3D--alD42mY6Y91hA1cu--0Y0mGzMhxxFqOo1MvHz47Q%3D%3D'
+    },
+    data : data
+  };
+  
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+  
 });
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
